f83415c1673350f2731c0145006baf7306df6867
maimas <maimas.andrei@gmail.com>
2020-01-12 23:13:45 -0800

    Added ability to create and query renter trough a REST endpoint. Added unit and integration tests.

--- sr-renter-service/src/main/java/com/mg/smartrent/renter/service/RenterService.java
+++ sr-renter-service/src/main/java/com/mg/smartrent/renter/service/RenterService.java
@@ -1,32 +1,45 @@
 package com.mg.smartrent.renter.service;
 
-import com.mg.persistence.domain.SystemFiled;
-import com.mg.persistence.domain.bizitem.model.BizItemModel;
-import com.mg.persistence.domain.bizitem.service.BizItemService;
-import com.mg.persistence.exceptions.BizItemSchemaValidationException;
-import com.mg.persistence.exceptions.ValidationSchemaNotFoundException;
-import com.mg.smartrent.renter.domain.Renter;
-import org.springframework.stereotype.Service;
 
+import com.mg.persistence.service.QueryService;
+import com.mg.smartrent.domain.models.Renter;
+import com.mg.smartrent.domain.validation.ModelValidationException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+import static com.mg.smartrent.domain.enrichment.ModelEnricher.enrich;
+import static com.mg.smartrent.domain.validation.ModelValidator.validate;
+
 @Service
+@Validated
 public class RenterService {
 
-    private BizItemService itemService;
+    private static final Logger log = LogManager.getLogger(RenterService.class);
 
-    public RenterService(BizItemService itemService) {
-        this.itemService = itemService;
+    private QueryService<Renter> queryService;
+
+
+    public RenterService(QueryService<Renter> queryService) {
+        this.queryService = queryService;
     }
 
 
-    public Renter save(Renter renter) throws ValidationSchemaNotFoundException, BizItemSchemaValidationException {
-        return (Renter) itemService.save(renter);
+    public Renter save(@NotNull Renter model) throws ModelValidationException {
+        enrich(model);
+        validate(model);
+        Renter renter = queryService.save(model);
+        log.info("Renter created. TrackingId = " + renter.getTrackingId());
+        return renter;
     }
 
-    public Renter findByTrackingId(String trackingId) {
-        List<BizItemModel> bizItemModels = itemService.find(SystemFiled.TrackingId, trackingId, Renter.class.getSimpleName());
-
-        return (bizItemModels == null || bizItemModels.isEmpty()) ? null : (Renter) bizItemModels.get(0);
+    public Renter findByEmail(@NotNull @Email String email) {
+        List<Renter> renters = queryService.findAllBy("email", email, Renter.class);
+        return (renters != null && !renters.isEmpty()) ? renters.get(0) : null;
     }
 }
