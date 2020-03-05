131d0775adca63b88c067f015d35ece249332c75
maimas <maimas.andrei@gmail.com>
2020-03-05 09:08:02 -0800

    Secured service registration - defaulted to a simple user and password. Added distributed logging support.

--- sr-gateway-api-service/src/main/java/com/mg/smartrent/gatewayapi/GatewayApiApplication.java
+++ sr-gateway-api-service/src/main/java/com/mg/smartrent/gatewayapi/GatewayApiApplication.java
@@ -8,8 +8,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class GatewayApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApiApplication.class, args);
+    }
+
+
 
 }
